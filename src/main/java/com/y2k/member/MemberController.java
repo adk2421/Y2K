996ef87798bd4483d8d259fb73611a6c2cac0dc0
yunyeoun <dndudns@gmail.com>
2023-11-02 10:45:12 +0900

    swagger api 적용

--- src/main/java/com/y2k/member/MemberController.java
+++ src/main/java/com/y2k/member/MemberController.java
@@ -7,10 +7,15 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
+import io.swagger.v3.oas.annotations.Hidden;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.servlet.http.HttpSession;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
 
+@Tag(name = "회원가입 API", description = "Swagger 테스트용 API")
 @RestController
 @RequestMapping("/member")
 @RequiredArgsConstructor
@@ -34,6 +39,8 @@ public class MemberController {
      *
      * return -> FAIL ( 오류 ) / 성공 시 SUCC
      */
+    @Operation(summary = "회원가입", description = "회원DB에 회원 객체를 INSERT 합니다.")
+    @Parameter(name = "memberModel", description = "회원 객체")
     @RequestMapping(value = "/join", method = RequestMethod.POST)
     @ResponseBody
     public String JoinMember(@RequestBody MemberModel memberModel) throws Exception {
@@ -47,7 +54,7 @@ public class MemberController {
         //     e.printStackTrace();
         //     return "FAIL";
         // }
-        return "SUCC " + result;
+        return "SUCC";
     }
 
 
@@ -62,6 +69,7 @@ public class MemberController {
      *
      * return -> FAIL ( 오류 ) / 성공 시 SUCC , 세션에 데이터 저장
      */
+    @Hidden
     @RequestMapping(value = "/login", method = RequestMethod.POST)
     @ResponseBody
     public String loginMember(@RequestBody MemberModel memberModel, HttpSession session) throws Exception {
@@ -79,6 +87,7 @@ public class MemberController {
 
     }
 
+    @Hidden
     @RequestMapping("test")
     @ResponseBody
     public String test() throws Exception {
--- src/main/java/com/y2k/member/MemberModel.java
+++ src/main/java/com/y2k/member/MemberModel.java
@@ -1,27 +1,32 @@
 package com.y2k.member;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Getter;
 import lombok.Setter;
 
 @Getter
 @Setter
+@Schema(name="Member", description = "회원")
 public class MemberModel {
 
 
+    @Schema(name = "memberId", description = "아이디", example = "kim")
     private String memberId;
 
+    @Schema(name = "memberPw", description = "비밀번호")
     private String memberPw;
 
+    @Schema(name = "memberName", description = "회원명", example = "홍길동길동")
+    private String memberName;
+
+    @Schema(name = "memberPhone", description = "전화번호", example = "010-1111-2222")
     private String memberPhone;
 
+    @Schema(name = "createDate", description = "등록일", example = "2023-11-01")
     private String createDate;
 
     public int getCount() {
         return 0;
-    }
-
-    public Object getMemberName() {
-        return null;
     }
 
     public Object getMemberNo() {
